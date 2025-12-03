085a8d96c7eec99be0009960e91a2b30a123b128
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2025-12-02 23:56:26 -0500

    final

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+from io import StringIO
+import re
+import pandas as pd # type: ignore
 
 def pregunta_01():
     """
@@ -18,3 +21,26 @@ def pregunta_01():
 
 
     """
+    with open("files/input/clusters_report.txt") as fp:
+        col1 = re.sub(r"\s{2,}", "\t", fp.readline().strip()).split("\t")
+        col2 = re.sub(r"\s{2,}", "\t", fp.readline()).split("\t")
+        fp.readline()
+        fp.readline()
+
+    columns = [str(col1[i] + " " + col2[i]).strip() if len(col2) > i else col1[i] for i in range(len(col1))]
+    cluster, cantidad, porcentaje, principales = [col.lower().replace(" ", "_") for col in columns]
+
+    df = pd.read_fwf("files/input/clusters_report.txt", skiprows=4, header=None)
+    df.columns = [cluster, cantidad, porcentaje, principales]
+
+    df = df.ffill()
+
+    df[porcentaje] = df[porcentaje].str.replace(",", ".").str.replace(" %", "").astype(float)
+
+    df = df.groupby([cluster, cantidad, porcentaje])[principales].agg(lambda x: x).reset_index(name=principales)
+
+    df[principales] = df[principales].str.join(" ").str.replace(r"\s{2,}", " ", regex=True).str.replace(".", "").str.strip()
+
+    return df
+
+print(pregunta_01())
